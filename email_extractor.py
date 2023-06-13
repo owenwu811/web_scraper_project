bde05704a6b31b821640be8c34af17e6bf5b3e05
Daniel Miller <danielemiller@live.com>
2023-06-13 14:49:59 +0000

    Added link_crawler.py script

---
+++ email_extractor.py
@@ -0,0 +1,32 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+
+output_dir = "/home/chatgpt/custom_utilities/utility_library/tmp/email_extractor_output"
+log_file = os.path.join(output_dir, "log.txt")
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+def log_message(message):
+    with open(log_file, "a") as log:
+        log.write(message + "\n")
+
+def extract_emails(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            page_content = response.text
+            soup = BeautifulSoup(page_content, 'html.parser')
+            email_addresses = re.findall(r"[a-z0-9\.\-_]+@[a-z0-9\.\-_]+\.[a-z]+", soup.text, re.I)
+            with open(os.path.join(output_dir, "output.txt"), "w") as output_file:
+                for email in email_addresses:
+                    output_file.write(email + "\n")
+            log_message(f"Successfully extracted {len(email_addresses)} email addresses from {url}")
+        else:
+            log_message(f"Failed to retrieve page content. Status code: {response.status_code}")
+    except Exception as e:
+        log_message(f"An error occurred: {str(e)}")
+
+extract_emails("https://10minutemail.com/")
---
+++ link_crawler.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import logging
+import sys
+from urllib.parse import urljoin, urlparse
+
+# Set up logging
+output_dir = "/home/chatgpt/custom_utilities/utility_library/tmp/link_crawler"
+os.makedirs(output_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(output_dir, "link_crawler_log.txt"), level=logging.INFO)
+
+def crawl_links(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logging.error(f"Error fetching {url}: {e}")
+        return
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract links
+    links = []
+    for a in soup.find_all("a", href=True):
+        link = urljoin(url, a["href"])
+        links.append(link)
+
+    # Write links to output file
+    with open(os.path.join(output_dir, "link_crawler_output.txt"), "w") as f:
+        for link in links:
+            f.write(link + "\n")
+
+    logging.info(f"Links for {url}: {links}")
+
+# Get URL from command-line argument
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    url = "http://example.com"
+
+# Test the function
+crawl_links(url)
