58fbe9733bf4f72f8650504bec9c96a676c5475b
Daniel Miller <danielemiller@live.com>
2023-06-13 13:50:45 +0000

    Updated css_js_extractor.py to handle full URLs

---
+++ css_js_extractor.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import logging
+import sys
+from urllib.parse import urljoin
+
+# Set up logging
+output_dir = "/home/chatgpt/custom_utilities/utility_library/tmp/css_js_extractor"
+os.makedirs(output_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(output_dir, "css_js_extractor_log.txt"), level=logging.INFO)
+
+def extract_css_js(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logging.error(f"Error fetching {url}: {e}")
+        return
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract CSS and JS files
+    css_js_files = []
+    for link in soup.find_all("link", rel="stylesheet"):
+        if "href" in link.attrs:
+            css_js_files.append(urljoin(url, link["href"]))
+    for script in soup.find_all("script"):
+        if "src" in script.attrs:
+            css_js_files.append(urljoin(url, script["src"]))
+
+    # Write CSS and JS files to output file
+    with open(os.path.join(output_dir, "css_js_extractor_output.txt"), "w") as f:
+        for file in css_js_files:
+            f.write(file + "\n")
+
+    logging.info(f"CSS and JS files for {url}: {css_js_files}")
+
+# Get URL from command-line argument
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    url = "http://example.com"
+
+# Test the function
+extract_css_js(url)
--- image_downloader.py
+++ image_downloader.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import logging
 import sys
 import base64
+from urllib.parse import urljoin
 
 # Set up logging
 dir_path = "/home/chatgpt/custom_utilities/utility_library/tmp/image_downloader"
@@ -20,13 +21,15 @@ def download_images(url):
         # For each image tag, download the image and save it to the directory
         for img in img_tags:
             img_url = img.get("src")
+            # Convert relative URLs to absolute URLs
+            img_url = urljoin(url, img_url)
             if "data:image" in img_url:
                 # If the image is a data URL, decode the data URL
                 img_data = base64.b64decode(img_url.split(",", 1)[1])
             else:
                 # Otherwise, send a GET request to the image URL
                 img_data = requests.get(img_url).content
-            img_file = os.path.join(dir_path, os.path.basename(img_url.split("/", -1)[-1]))
+            img_file = os.path.join(dir_path, "image_web_server", os.path.basename(img_url.split("/", -1)[-1]))
             with open(img_file, "wb") as f:
                 f.write(img_data)
             logging.info(f"Downloaded image {img_file}")
